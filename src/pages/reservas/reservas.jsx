260472e82b6ac2332550c68a7d4f696566ed9dd0
Marcos Pérez <mperez17kk@gmail.com>
2023-12-18 18:39:20 +0000

    reservations formated

--- src/pages/reservas/reservas.jsx
+++ src/pages/reservas/reservas.jsx
@@ -1,27 +1,23 @@
-import './reservas.css';
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import TextField from '@mui/material/TextField';
+import "./reservas.css";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 
-const timeSlots = [
-  '14:00 - 15:00',
-  '15:00 - 16:00',
-  '16:00 - 17:00'
-];
+const timeSlots = ["14:00 - 15:00", "15:00 - 16:00", "16:00 - 17:00"];
 
 function App() {
   const [bookings, setBookings] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
   const [reservationDetails, setReservationDetails] = useState({
-    timeSlot: '',
-    numberOfPeople: '',
+    timeSlot: "",
+    numberOfPeople: "",
   });
 
   const handleBookings = (value) => {
@@ -40,37 +36,37 @@ function App() {
   };
 
   const handleConfirmReservation = () => {
-    console.log('Fecha:', selectedDate);
-    console.log('Franja horaria:', reservationDetails.timeSlot);
-    console.log('Número de personas:', reservationDetails.numberOfPeople);
+    console.log("Fecha:", selectedDate);
+    console.log("Franja horaria:", reservationDetails.timeSlot);
+    console.log("Número de personas:", reservationDetails.numberOfPeople);
     setOpenModal(false);
   };
 
   return (
-    <div className='app'>
-      <h1 className='text-center'>Escoge fecha</h1>
-      <div className='calendar-container'>
+    <div className="app">
+      <h1 className="text-center">Escoge fecha</h1>
+      <div className="calendar-container">
         <Calendar onChange={handleBookings} />
       </div>
-
-      {/* Modal for reservation details */}
       <Dialog
         open={openModal}
         onClose={handleCloseModal}
-        aria-labelledby='alert-dialog-title'
-        aria-describedby='alert-dialog-description'
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id='alert-dialog-title'>Reservar Franja Horaria</DialogTitle>
+        <DialogTitle id="alert-dialog-title">
+          Reservar Franja Horaria
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText id='alert-dialog-description'>
+          <DialogContentText id="alert-dialog-description">
             Por favor, completa los detalles de tu reserva:
           </DialogContentText>
           <TextField
             fullWidth
-            margin='normal'
+            margin="normal"
             select
-            label='Franja Horaria'
-            name='timeSlot'
+            label="Franja Horaria"
+            name="timeSlot"
             value={reservationDetails.timeSlot}
             onChange={handleReservationDetailsChange}
           >
@@ -82,19 +78,19 @@ function App() {
           </TextField>
           <TextField
             fullWidth
-            margin='normal'
-            label='Número de Personas'
-            name='numberOfPeople'
-            type='number'
+            margin="normal"
+            label="Número de Personas"
+            name="numberOfPeople"
+            type="number"
             value={reservationDetails.numberOfPeople}
             onChange={handleReservationDetailsChange}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleCloseModal} color='primary'>
+          <Button onClick={handleCloseModal} color="primary">
             Cancelar
           </Button>
-          <Button onClick={handleConfirmReservation} color='primary' autoFocus>
+          <Button onClick={handleConfirmReservation} color="primary" autoFocus>
             Confirmar Reserva
           </Button>
         </DialogActions>
